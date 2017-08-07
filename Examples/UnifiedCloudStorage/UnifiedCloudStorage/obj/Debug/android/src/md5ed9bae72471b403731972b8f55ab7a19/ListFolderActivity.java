255fd42138a65e19e9a6fa09193a2b8496b03151
Mujtaba Alam <mujtaba.alam@cloudrail.com>
2017-08-07 21:45:33 +0200

    Initial commit

---
+++ Examples/UnifiedCloudStorage/UnifiedCloudStorage/obj/Debug/android/src/md5ed9bae72471b403731972b8f55ab7a19/ListFolderActivity.java
@@ -0,0 +1,92 @@
+package md5ed9bae72471b403731972b8f55ab7a19;
+
+
+public class ListFolderActivity
+	extends android.app.ListActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onCreateOptionsMenu:(Landroid/view/Menu;)Z:GetOnCreateOptionsMenu_Landroid_view_Menu_Handler\n" +
+			"n_onListItemClick:(Landroid/widget/ListView;Landroid/view/View;IJ)V:GetOnListItemClick_Landroid_widget_ListView_Landroid_view_View_IJHandler\n" +
+			"n_onOptionsItemSelected:(Landroid/view/MenuItem;)Z:GetOnOptionsItemSelected_Landroid_view_MenuItem_Handler\n" +
+			"n_onNewIntent:(Landroid/content/Intent;)V:GetOnNewIntent_Landroid_content_Intent_Handler\n" +
+			"n_onActivityResult:(IILandroid/content/Intent;)V:GetOnActivityResult_IILandroid_content_Intent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("UnifiedCloudStorage.ListFolderActivity, UnifiedCloudStorage, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", ListFolderActivity.class, __md_methods);
+	}
+
+
+	public ListFolderActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == ListFolderActivity.class)
+			mono.android.TypeManager.Activate ("UnifiedCloudStorage.ListFolderActivity, UnifiedCloudStorage, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public boolean onCreateOptionsMenu (android.view.Menu p0)
+	{
+		return n_onCreateOptionsMenu (p0);
+	}
+
+	private native boolean n_onCreateOptionsMenu (android.view.Menu p0);
+
+
+	public void onListItemClick (android.widget.ListView p0, android.view.View p1, int p2, long p3)
+	{
+		n_onListItemClick (p0, p1, p2, p3);
+	}
+
+	private native void n_onListItemClick (android.widget.ListView p0, android.view.View p1, int p2, long p3);
+
+
+	public boolean onOptionsItemSelected (android.view.MenuItem p0)
+	{
+		return n_onOptionsItemSelected (p0);
+	}
+
+	private native boolean n_onOptionsItemSelected (android.view.MenuItem p0);
+
+
+	public void onNewIntent (android.content.Intent p0)
+	{
+		n_onNewIntent (p0);
+	}
+
+	private native void n_onNewIntent (android.content.Intent p0);
+
+
+	public void onActivityResult (int p0, int p1, android.content.Intent p2)
+	{
+		n_onActivityResult (p0, p1, p2);
+	}
+
+	private native void n_onActivityResult (int p0, int p1, android.content.Intent p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ Examples/UnifiedCloudStorage/UnifiedCloudStorage/obj/Release/android/src/md5ed9bae72471b403731972b8f55ab7a19/ListFolderActivity.java
@@ -0,0 +1,65 @@
+package md5ed9bae72471b403731972b8f55ab7a19;
+
+
+public class ListFolderActivity
+	extends android.app.ListActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onListItemClick:(Landroid/widget/ListView;Landroid/view/View;IJ)V:GetOnListItemClick_Landroid_widget_ListView_Landroid_view_View_IJHandler\n" +
+			"n_onNewIntent:(Landroid/content/Intent;)V:GetOnNewIntent_Landroid_content_Intent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("UnifiedCloudStorage.ListFolderActivity, UnifiedCloudStorage, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", ListFolderActivity.class, __md_methods);
+	}
+
+
+	public ListFolderActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == ListFolderActivity.class)
+			mono.android.TypeManager.Activate ("UnifiedCloudStorage.ListFolderActivity, UnifiedCloudStorage, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onListItemClick (android.widget.ListView p0, android.view.View p1, int p2, long p3)
+	{
+		n_onListItemClick (p0, p1, p2, p3);
+	}
+
+	private native void n_onListItemClick (android.widget.ListView p0, android.view.View p1, int p2, long p3);
+
+
+	public void onNewIntent (android.content.Intent p0)
+	{
+		n_onNewIntent (p0);
+	}
+
+	private native void n_onNewIntent (android.content.Intent p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
